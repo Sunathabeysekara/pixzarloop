6a8cce9c30f46fc81c76ab5c6d19864575a46ce0
Sunath Abeysekara <abeysekarasunath666@gmail.com>
2025-02-22 15:48:21 +0530

    index for admin login
    for login admin to system

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login page</title>
+	<!--a admin login page id:1,password:admin -->
+</head>
+<body>
+
+<header>
+</header>
+
+<form method="POST" action="index.php">
+	<h2  style="text-align: center; ">Admin Login</h2>
+
+    <!--"user" -->
+    <input type="text" id="userid" style="text-align: center;margin-left:680px " name="userid" placeholder="ID" required><br>
+    <br>
+    <!--"password"-->
+    <input type="password"	style="text-align:center;margin-left:680px" id="password" name="password" placeholder="Password" required><br>
+    
+ 
+    <input type="submit" value="Log In" style="margin-left: 740px;margin-top: 12px;">
+ 
+</form>
+<p>id:1<br>password:admin</p>
+<?php
+require_once'config/connect.php';
+require_once'index.php';
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+	$username=$_POST['userid'];
+	$password=$_POST['password'];
+
+	$query="select id,role from users where id'$userid' and password='$password'";
+	$result=mysqli_query($connect,$query);
+	$num=mysqli_num_rows($result);
+
+	//for if login correctly
+	if($num==1){
+
+			header('location:home.php');
+	}else{
+		echo '<p style="color: red";>wrong email or password</p>';
+	}
+
+
+}
+
+?>
+
+<footer>
+</footer>
+</body>
+</html>
